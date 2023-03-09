01d5a220f771d30f8077ae3587e2e2b12dd7f96b
anaandjelic <ana.andjelic161@gmail.com>
2023-03-09 22:06:11 +0100

    Add translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,41 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { LanguageToggleComponent } from './components/language-toggle/language-toggle.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { MapComponent } from './components/map/map.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
+    LanguageToggleComponent,
     MapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SlickCarouselModule,
+    HttpClientModule,
     LeafletModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'sr',
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    })
   ],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
